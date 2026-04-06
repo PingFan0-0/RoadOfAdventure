024b28ddde7d232cf80ae6eba7aa19d60807ea7b
PingFan0-0 <2392756994@qq.com>
2026-04-06 14:34:47 +0800

    Create Model.h

---
+++ RoadOfAdventure/Model.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+
+extern float verticesZ[];
+extern const int verticesZ_size;
+
+extern unsigned int indicesZ[];
+extern const int indicesZ_size;
+
+extern unsigned int propZ[];
+extern const int propZ_size;
+
+
+
