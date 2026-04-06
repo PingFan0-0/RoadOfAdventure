cd9b98720fb089dcd4c070493569a4efd289a4eb
PingFan0-0 <2392756994@qq.com>
2026-04-06 14:34:43 +0800

    OpenGL

---
+++ RoadOfAdventure/OpenGL.cpp
@@ -0,0 +1,86 @@
+﻿#include "OpenGL.h"
+
+#include"GameData.h"
+#include"Debug.h"
+#include<fstream>
+#include"TypeConversion.h"
+
+
+bool BoolMod = false;//是否线框模式
+GLFWwindow* window;//窗口对象
+
+
+
+bool GLBegin() {//窗口初始化
+	glfwInit();
+	
+	// 设置 OpenGL 版本
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
+	
+	// 初始化窗口
+	window = glfwCreateWindow(Win.WinX, Win.WinY, "RoadOfAdventure", nullptr, nullptr);
+	if (!window) {
+	    glfwTerminate();
+	    return false;
+	}
+	glfwMakeContextCurrent(window);
+	
+	// 初始化GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		return false;
+	}
+
+	return true;
+}
+
+void GLEnd() {//关闭窗口
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+
+void OpenGLMousePos() {//更新鼠标位置
+	glfwGetCursorPos(window, &MouseX, &MouseY);
+}
+//打开着色器文件
+#pragma region
+std::string LoadShader(std::string name) {
+	Debug("读取着色器 " + name);
+	std::string ret = "";
+	std::ifstream PF(wstring_string(DataParent + L"/" + ShaderParent) + "/" + name);//打开文件
+	if (PF.is_open()) {//文件是否打开
+		std::stringstream buf;
+		buf << PF.rdbuf();//写入文件
+		ret = buf.str();//读取字符
+	}
+	else {
+		Error("着色器打开失败 " + name, "W");
+	}
+	PF.close();//关闭
+	return ret;
+}
+#pragma endregion
+
+
+//void GLDrawText(float x, float y, const std::string& text, void* font, float r, float g, float b) {
+//	// 保存当前颜色和矩阵状态
+//	glPushMatrix();
+//	glPushAttrib(GL_CURRENT_BIT);
+//
+//	// 设置颜色
+//	glColor3f(r, g, b);
+//
+//	// 设置光栅位置（世界坐标）
+//	glRasterPos2f(x, y);
+//
+//	// 逐个字符绘制
+//	for (char c : text) {
+//		glutBitmapCharacter(font, c);
+//	}
+//
+//	// 恢复状态
+//	glPopAttrib();
+//	glPopMatrix();
+//}
---
+++ RoadOfAdventure/OpenGL.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "stb_image.h"
+#include "stb_image_resize.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H 
+
+
+//#include "Image.h"
+//#include "Model.h"
+//#include "Shader.h"
+//#include "TextRenderer.h"
+//#include "Texture.h"
+//#include "Vertex.h"
+
+
+#include <string>
+#include <map>
+
+
+extern bool BoolMod;
+extern GLFWwindow* window;
+
+struct XYInt {
+	int X, Y;
+};
+
+//窗口
+
+bool GLBegin();//窗口初始化
+void GLEnd();//关闭窗口
+
+
+void OpenGLMousePos();//更新鼠标位置
+
+std::string LoadShader(std::string name);//打开着色器文件
